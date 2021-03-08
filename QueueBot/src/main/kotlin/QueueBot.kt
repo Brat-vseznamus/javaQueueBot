f9d89b55247e1ae8ca8842da79d85ee9a270765e
Brat-vseznamus <fvixnin@gmail.com>
2021-03-08 19:20:28 +0300

    small changes

--- QueueBot/src/main/kotlin/QueueBot.kt
+++ QueueBot/src/main/kotlin/QueueBot.kt
@@ -1,27 +1,8 @@
-import dev.inmo.micro_utils.coroutines.safely
-import dev.inmo.tgbotapi.extensions.api.chat.get.getChat
-import dev.inmo.tgbotapi.extensions.api.send.reply
-import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
 import dev.inmo.tgbotapi.bot.Ktor.telegramBot
 import dev.inmo.tgbotapi.extensions.api.bot.getMe
-import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviour
-import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
-import dev.inmo.tgbotapi.extensions.utils.asChannelChat
-import dev.inmo.tgbotapi.extensions.utils.formatting.linkMarkdownV2
-import dev.inmo.tgbotapi.extensions.utils.formatting.textMentionMarkdownV2
-import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.startGettingFlowsUpdatesByLongPolling
-import dev.inmo.tgbotapi.types.BotCommand
-import dev.inmo.tgbotapi.types.ChatIdentifier
-import dev.inmo.tgbotapi.types.ParseMode.Markdown
-import dev.inmo.tgbotapi.types.ParseMode.MarkdownV2
-import dev.inmo.tgbotapi.types.User
-import dev.inmo.tgbotapi.types.chat.abstracts.*
-import dev.inmo.tgbotapi.types.textField
-import dev.inmo.tgbotapi.utils.extensions.escapeMarkdownV2Common
+import dev.inmo.tgbotapi.utils.PreviewFeature
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.FileInputStream
@@ -29,9 +10,6 @@ import java.io.InputStreamReader
 import java.net.URL
 import java.util.*
 
-/**
- * The main purpose of this bot is just to answer "Oh, hi, " and add user mention here
- */
 
 internal val db : DB = DB()
 fun sendMessage(chatId : Int, text : String, botToken : String) {
@@ -43,15 +21,7 @@ fun sendMessage(chatId : Int, text : String, botToken : String) {
     val response = br.readText()
 }
 
-//fun sendRequestToStart(chatId : Int, text : String, botToken : String) {
-//    var urlString = "https://api.telegram.org/bot%s/start"
-//    urlString = String.format(urlString, botToken)
-//    val conn = URL(urlString).openConnection()
-//    val inputStream = BufferedInputStream(conn.getInputStream())
-//    val br = BufferedReader(InputStreamReader(inputStream))
-//    val response = br.readText()
-//}
-
+@OptIn(PreviewFeature::class)
 suspend fun main(vararg args: String) {
     val botproperties = Properties();
     botproperties.load(FileInputStream("build/resources/main/botInfo.properties"))
@@ -72,7 +42,6 @@ suspend fun main(vararg args: String) {
         muteCommand()
         helpCommand()
     }
-
 
     GlobalScope.launch {
         while (true) {
